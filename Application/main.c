6a36957fb31b7e98e78a72d45ea8cc0ba30bbc36
ylongwang2782 <ylongwangi@qq.com>
2024-12-03 17:33:24 +0800

    first commit

---
+++ Application/main.c
@@ -0,0 +1,36 @@
+#include "gd32f4xx.h"
+#include "gd32f470v_start.h"
+#include "systick.h"
+#include <stdio.h>
+
+/*!
+    \brief      main function
+    \param[in]  none
+    \param[out] none
+    \retval     none
+*/
+int main(void)
+{
+    /* configure systick */
+    systick_config();
+
+    /* enable the LEDs GPIO clock */
+    rcu_periph_clock_enable(RCU_GPIOC);
+
+    /* configure LED2 GPIO port */
+    gpio_mode_set(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO_PIN_6);
+    gpio_output_options_set(GPIOC, GPIO_OTYPE_PP, GPIO_OSPEED_50MHZ, GPIO_PIN_6);
+    /* reset LED2 GPIO pin */
+    gpio_bit_reset(GPIOC, GPIO_PIN_6);
+
+    while (1)
+    {
+        /* turn on LED2 */
+        gpio_bit_set(GPIOC, GPIO_PIN_6);
+        delay_1ms(1000);
+
+        /* turn off LED2 */
+        gpio_bit_reset(GPIOC, GPIO_PIN_6);
+        delay_1ms(1000);
+    }
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+# GD32F4xx_keil_project_template
+
+## Introduction
+
+这是一个基于GD32F4xx官方文件的keil工程模板文件，可以开箱即用
